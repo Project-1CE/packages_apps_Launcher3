Project-1CE/packages_apps_Launcher3

commit 72c0a7994ceeea1e9d944728976c1d064860b208

Launcher3: Fix icon palette crashes
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Change-Id: I7d641ec3e2cb2f8ad11a16df8143c32a6deb2d59

src/com/android/launcher3/graphics/IconPalette.java
@@ -98,8 +98,8 @@ public class IconPalette {
     /** For debugging. This was copied from com.android.internal.util.NotificationColorUtil. */
     private static String contrastChange(int colorOld, int colorNew, int bg) {
         return String.format("from %.2f:1 to %.2f:1",
-                ColorUtils.calculateContrast(colorOld, bg),
-                ColorUtils.calculateContrast(colorNew, bg));
+                calculateContrast(colorOld, bg),
+                calculateContrast(colorNew, bg));
     }
 
     /**
@@ -123,7 +123,7 @@ public class IconPalette {
      * This was copied from com.android.internal.util.NotificationColorUtil.
      */
     private static int findContrastColor(int fg, int bg, double minRatio) {
-        if (ColorUtils.calculateContrast(fg, bg) >= minRatio) {
+        if (calculateContrast(fg, bg) >= minRatio) {
             return fg;
         }
 
@@ -139,13 +139,22 @@ public class IconPalette {
         for (int i = 0; i < 15 && high - low > 0.00001; i++) {
             final double l = (low + high) / 2;
             fg = ColorUtils.LABToColor(l, a, b);
-            if (ColorUtils.calculateContrast(fg, bg) > minRatio) {
+            if (calculateContrast(fg, bg) > minRatio) {
                 if (isBgDark) high = l; else low = l;
             } else {
                 if (isBgDark) low = l; else high = l;
             }
         }
         return ColorUtils.LABToColor(low, a, b);
+    }
+
+    private static double calculateContrast(int fg, int bg) {
+        bg = setAlphaComponent(bg, 255);
+        return ColorUtils.calculateContrast(fg, bg);
+    }
+
+    private static int setAlphaComponent(int color, int alpha) {
+        return (color & 0x00ffffff) | (alpha << 24);
     }
 
     public static int getMutedColor(int color, float whiteScrimAlpha) {

commit 205535f99d7c4fe6aea496e3b6d1f77ccfaf24e2

fixup! Launcher3: Fix icon palette crashes
 * Call setAlphaComponent() from ColorUtils.

Fixes: 72c0a79 ("Launcher3: Fix icon palette crashes")
Change-Id: Ie3e7193473184c8351c309ccfc0eee58519d3475

src/com/android/launcher3/graphics/IconPalette.java
@@ -149,12 +149,7 @@ public class IconPalette {
     }
 
     private static double calculateContrast(int fg, int bg) {
-        bg = setAlphaComponent(bg, 255);
-        return ColorUtils.calculateContrast(fg, bg);
-    }
-
-    private static int setAlphaComponent(int color, int alpha) {
-        return (color & 0x00ffffff) | (alpha << 24);
+        return ColorUtils.calculateContrast(fg, ColorUtils.setAlphaComponent(bg, 255));
     }
 
     public static int getMutedColor(int color, float whiteScrimAlpha) {